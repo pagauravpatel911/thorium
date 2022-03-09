77c9a87c3ced402bfb56789dfacd11e1eff1f8ee
gaurav <pgauravpatel911@gmail.com>
2022-03-09 08:48:16 +0530

    token Assignment

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,14 +1,14 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
-const createUser = async function (abcd, xyz) {
+const createUser = async function (req, res) {
   //You can name the req, res objects anything.
   //but the first parameter is always the request 
   //the second parameter is always the response
-  let data = abcd.body;
+  let data = req.body;
   let savedData = await userModel.create(data);
-  console.log(abcd.newAtribute);
-  xyz.send({ msg: savedData });
+  
+  res.send({ msg: savedData });
 };
 
 const loginUser = async function (req, res) {
@@ -31,10 +31,10 @@ const loginUser = async function (req, res) {
   let token = jwt.sign(
     {
       userId: user._id.toString(),
-      batch: "thorium",
+      batch: "First-token",
       organisation: "FUnctionUp",
     },
-    "functionup-thorium"
+    "functionup-thorium-myToken"
   );
   res.setHeader("x-auth-token", token);
   res.send({ status: true, data: token });
@@ -47,14 +47,14 @@ const getUserData = async function (req, res) {
   //If no token is present in the request header return error
   if (!token) return res.send({ status: false, msg: "token must be present" });
 
-  console.log(token);
+ // console.log(token);
   
   // If a token is present then decode the token with verify function
   // verify takes two inputs:
   // Input 1 is the token to be decoded
   // Input 2 is the same secret with which the token was generated
   // Check the value of the decoded token yourself
-  let decodedToken = jwt.verify(token, "functionup-thorium");
+  let decodedToken = jwt.verify(token, "functionup-thorium-myToken");
   if (!decodedToken)
     return res.send({ status: false, msg: "token is invalid" });
 
@@ -80,11 +80,31 @@ const updateUser = async function (req, res) {
   }
 
   let userData = req.body;
-  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
+  console.log(userData)
+  let updatedUser = await userModel.findByIdAndUpdate({ _id: userId }, userData);
   res.send({ status: updatedUser, data: updatedUser });
 };
+const deleteUser = async function (req, res) {
+  // Do the same steps here:
+  // Check if the token is present
+  // Check if the token present is a valid token
+  // Return a different error message in both these cases
+  
+    let userId = req.params.userId;
+    let user = await userModel.findById(userId);
+    //Return an error if no user with the given id exists in the db
+    if (!user) {
+      return res.send("No such user exists");
+    }
+  
+    let userData = req.body;
+    console.log(userData)
+    let updatedUser = await userModel.findByIdAndUpdate({ _id: userId }, userData);
+    res.send({ status: updatedUser, data: updatedUser });
+  };
 
 module.exports.createUser = createUser;
 module.exports.getUserData = getUserData;
 module.exports.updateUser = updateUser;
 module.exports.loginUser = loginUser;
+module.exports.deleteUser = deleteUser;
--- src/routes/route.js
+++ src/routes/route.js
@@ -6,7 +6,7 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/users", userController.createUser  )
+router.post("/users", userController.createUser)
 
 router.post("/login", userController.loginUser)
 
@@ -14,5 +14,6 @@ router.post("/login", userController.loginUser)
 router.get("/users/:userId", userController.getUserData)
 
 router.put("/users/:userId", userController.updateUser)
+router.delete("/users/:userId", userController.deleteUser)
 
 module.exports = router;
